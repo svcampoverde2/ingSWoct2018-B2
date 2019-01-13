88448d6fe99eca24502d3a460eb29d27883e5d53
svcampoverde2 <46578256+svcampoverde2@users.noreply.github.com>
2019-01-13 01:00:21 -0500

    Agregamos un formato para almacenar datos.version2

--- Practicas1/src/ingswoct2018/Practicas1.java
+++ Practicas1/src/ingswoct2018/Practicas1.java
@@ -1,22 +1,30 @@
 
 package ingswoct2018;
 
+import java.util.Formatter;
+import java.io.File;
 import javax.swing.JOptionPane;
-
+import java.io.IOException;
 public class Practicas1 {
 
-    
     public static void main(String[] args) {
+         try{
+    Formatter outArchivo = new Formatter("Mostrar edad.csv"); 
         int año_actual, año_nacimiento,edad;
          año_actual = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Año Actual: "));
          año_nacimiento = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Año que nacio: "));
         edad = año_actual - año_nacimiento;
+        outArchivo.format(" actual; nacio;edad\n");
          if(edad > 90 || edad < 0){
        JOptionPane.showMessageDialog(null," ingresae datos nuevamente:");
     }else {
         JOptionPane.showMessageDialog(null, "Su Edad es:" +edad);
          }
-        System.out.print("Version 1.0");  
+       outArchivo.format("%d;%d;%d \n ",año_actual,año_nacimiento,edad);
+        outArchivo.close();
+    }catch(Exception e){
+     System.out.print(e);
+    }  
     }
     
 }
