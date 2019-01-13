49e3ea2dd80eda043fdc1eaf37f8d042b2001676
svcampoverde2 <46578256+svcampoverde2@users.noreply.github.com>
2019-01-13 00:31:21 -0500

    Calcular edad

---
+++ Practicas1/src/ingswoct2018/Practicas1.java
@@ -0,0 +1,18 @@
+
+package ingswoct2018;
+
+import javax.swing.JOptionPane;
+
+public class Practicas1 {
+
+    
+    public static void main(String[] args) {
+        int año_actual, año_nacimiento,edad;
+         año_actual = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Año Actual: "));
+         año_nacimiento = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Año que nacio: "));
+        edad = año_actual - año_nacimiento;
+        JOptionPane.showMessageDialog(null, "Su Edad es:" +edad);
+        System.out.print("Version 1.0");  
+    }
+    
+}
