dd7fd1fb9f6404a5d6c8c0a221bc69b175a230b7
svcampoverde2 <46578256+svcampoverde2@users.noreply.github.com>
2019-01-13 01:27:25 -0500

    Serie fibonacci solicita cuantos terminos desea visualizar el usuario

--- Practicas1/src/ingswoct2018/Fibonacci.java
+++ Practicas1/src/ingswoct2018/Fibonacci.java
@@ -1,20 +1,28 @@
 
 package ingswoct2018;
-
+import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
         int num1 = 0;
         int num2 = 1;
         int temp;
-        int limite;
-        limite = 200;
+        int post = 1;
+        int tope = 0;
+        
+     //Pedimos al usuario que ingrese los numeros que desea visualizar
+        System.out.print("Ingrese el numero de terminos fibonacci: ");
+        tope = leer.nextInt();
         System.out.print(num1);
-        System.out.print(num2);
-        while (num1 + num2 <= limite) {
+        post++;
+        System.out.print(","+num2);
+        post++;
+        while (post <= tope) { 
             temp = num1;
             num1 = num2;
             num2 = temp + num1;
            System.out.print(","+num2);
+           post++;
         }
          System.out.println();
     }
