d40c0f2bc6be16703be3d392aa8b833e75a2fded
svcampoverde2 <46578256+svcampoverde2@users.noreply.github.com>
2019-01-14 15:43:29 -0500

    Solicita al usuario que ingrese el numero por teclado para calcular su factorial

--- Practicas1/src/ingswoct2018/calculaFactorial.java
+++ Practicas1/src/ingswoct2018/calculaFactorial.java
@@ -1,10 +1,12 @@
 
 package ingswoct2018;
-
+import java.util.Scanner;
 public class calculaFactorial {
    public static void main(String[]args){
+       Scanner leer = new Scanner(System.in);
        int factorial = 1;
-       int numero = 5;
+       System.out.printf("Ingrese un numero: ");
+       int numero = leer.nextInt();
        while(numero != 0){
        factorial *= numero;
        numero--;
